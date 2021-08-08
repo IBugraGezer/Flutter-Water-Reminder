ea3b36bbbc9b6f22f1c80c4d62847c516512fa2c
İbrahim Buğra Gezer <gezerbugra@gmail.com>
2021-08-08 17:09:09 +0300

    remove import

--- lib/view/settings/settings_view.dart
+++ lib/view/settings/settings_view.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_spinbox/flutter_spinbox.dart';
 
 class Settings extends StatefulWidget {
   const Settings({Key? key}) : super(key: key);
