b110cbdf035369b5314a025ddbc8a021d00db7fc
İbrahim Buğra Gezer <gezerbugra@gmail.com>
2021-08-08 17:08:28 +0300

    create settings page design

--- lib/view/settings/settings_view.dart
+++ lib/view/settings/settings_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_spinbox/flutter_spinbox.dart';
 
 class Settings extends StatefulWidget {
   const Settings({Key? key}) : super(key: key);
@@ -8,10 +9,64 @@ class Settings extends StatefulWidget {
 }
 
 class _SettingsState extends State<Settings> {
+  TextEditingController goalCounterController = TextEditingController();
+  int goalCounterValue = 0;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    goalCounterController.text = "0";
+  }
+
   @override
   Widget build(BuildContext context) {
+    Size deviceSize = MediaQuery.of(context).size;
     return Container(
-      child: Text("SA"),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            "Daily Goal",
+            style: Theme.of(context)
+                .textTheme
+                .headline5!
+                .copyWith(color: Colors.black),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              IconButton(
+                onPressed: () {
+                  setState(() {
+                    goalCounterValue += 1;
+                    goalCounterController.text = goalCounterValue.toString();
+                  });
+                },
+                icon: Icon(Icons.add),
+              ),
+              Container(
+                  width: deviceSize.width * 0.50,
+                  child: TextField(
+                      controller: goalCounterController,
+                      enabled: false,
+                      textAlign: TextAlign.center,
+                      style: Theme.of(context).textTheme.headline6)),
+              IconButton(
+                onPressed: () {
+                  setState(() {
+                    goalCounterValue -= 1;
+                    goalCounterController.text = goalCounterValue.toString();
+                  });
+                },
+                icon: Icon(Icons.remove),
+              ),
+            ],
+          )
+        ],
+      ),
     );
   }
 }
